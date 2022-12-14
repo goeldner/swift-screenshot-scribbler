71bebfbf2b26f617b3a1e9b195646e78918b78cb
goeldner <ch@goeldner.ch>
2022-12-14 23:01:50 +0100

    Move example images into separate folder

--- examples.sh
+++ examples.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# Default 1: all optional options ommitted
 swift run scrscr \
     --caption "Example output with default options and long caption" \
-    --input example-input.png \
-    --output example-output-default.png \
+    --input Examples/example-input.png \
+    --output Examples/example-output-default-1.png
+
+# Default 2: all options explicitly defined
+swift run scrscr \
+    --caption "Example output with default options and long caption" \
+    --input Examples/example-input.png \
+    --output Examples/example-output-default-2.png \
     --layout-type "text-before-image" \
     --text-area-ratio 0.25 \
     --image-size-reduction 0.85 \
@@ -13,13 +20,13 @@ swift run scrscr \
     --font-style "Bold" \
     --font-size 32 \
     --shadow-color "#000000" \
-    --shadow-size 5 \
-    --verbose
+    --shadow-size 5
 
+# Smaller text area and fully visible screenshot with dark background
 swift run scrscr \
     --caption "Dark example with full screenshot" \
-    --input example-input.png \
-    --output example-output-custom.png \
+    --input Examples/example-input.png \
+    --output Examples/example-output-full-screenshot.png \
     --layout-type "text-before-image" \
     --text-area-ratio 0.15 \
     --image-size-reduction 0.80 \
@@ -28,6 +35,4 @@ swift run scrscr \
     --font-name "Futura" \
     --font-style "Bold" \
     --font-size 28 \
-    --shadow-color "#000000" \
-    --shadow-size 0 \
-    --verbose
+    --shadow-size 0
